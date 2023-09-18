ac7693195e4315f9f6c3eab16605f48c5c5292fa
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-09-18 16:39:00 +0100

    React to-do app

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reactLogo from './images/react.svg';
+import './styles/app.scss';
+
+const rootElement = document.getElementById('root');
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+    <div>
+        <h1>Hello from React application</h1>
+        <img src={reactLogo} className="logo react" alt="React logo" />
+    </div>
+);
+
