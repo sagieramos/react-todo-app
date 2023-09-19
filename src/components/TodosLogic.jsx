1b320ffc3ad76e0c57a7ab68115e93616b519c0c
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-09-19 02:06:08 +0100

    added readme file

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import TodosList from './TodosList';
 import InputTodo from './InputTodo';
-// other imported components here
 
 const TodosLogic = () => {
   const [todos, setTodos] = useState([
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reactLogo from './images/react.svg';
 import './styles/app.scss';
 import TodosLogic from './components/TodosLogic';
 
@@ -10,7 +9,5 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <div>
     <TodosLogic />
-    <h1>Hello from React application</h1>
-    <img src={reactLogo} className="logo react" alt="React logo" />
   </div>,
 );
